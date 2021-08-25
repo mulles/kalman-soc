ce3bcf18b6adcc89824aaf20fc898a22e1a1c308
mulles <github.com@emu.lu>
2021-08-25 12:16:24 +0200

    -made the project build without external meson.build file -removed the build directories /build and /builddir and added them to .gitignore -updated and removed the script to run all automatically, will add it to the readme

--- downloadBuildKalmanSOC.sh
+++ downloadBuildKalmanSOC.sh
@@ -1,24 +1,13 @@
-#This Script builds kalman-soc Algo
+#This Script clones and builds kalman-soc Algo
 #Resource:https://mesonbuild.com/Tutorial.html#the-humble-beginning
 
 # Install Meson (Build Systems) and it's Dependencies 
-sudo apt install meson ninja-build build-essential clang-format
+sudo apt install meson ninja-build build-essential clang-format cmake
 
-# Clone Git Repo to build including subprojects cpputests located folder /dep/
-git clone --recursive git@github.com:okrasolar/kalman-soc.git
-
-cd kalman-soc/dep/cpputest/
-
-# Get missing meson.build file for cpputest subproject
-wget https://raw.githubusercontent.com/hofab/meson-cpputest/master/subprojects/cpputest/meson.build
-cd ..
-cd ..
-
-# Adjust main meson.build to work with the above cpputest meson.build file
-sed -i ''s/'cpputest_dep'/'lcpp_dep'/g''  meson.build
-
-meson setup builddir
-cd builddir
+# Clone Git Repo to build including subprojects cpputests located in folder /dep/
+git clone --recursive git@github.com:mulles/kalman-soc.git
+cd kalman-soc
+meson setup build
+cd build
 ninja
-
 
